b097ab3c726730e9b41466c06ba557346494caaa
LuizFelipe11 <lfcsoares04@gmail.com>
2025-03-08 04:04:57 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    MatButtonModule,
+    CommonModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  mostraBoasVindas: boolean = true;
+  mostraPrincipal: boolean = false;
+
+  protected iniciaQuestionario() {
+    this.mostraBoasVindas = false;
+    this.mostraPrincipal = true;
+  }
+
+}
